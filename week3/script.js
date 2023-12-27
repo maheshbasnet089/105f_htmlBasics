a799523b6f33d9e35dd38e843599b087b51d1146
Manish Basnet <manishbasnet@Manishs-MacBook-Air.local>
2023-12-27 20:42:54 +0545

    start JS

---
+++ week3/script.js
@@ -0,0 +1,19 @@
+var cup = "coffee" // string
+var collegeBag = 'book' // string
+var numberOfChauChau = '2' // string
+var isNepali = true // boolean
+
+cup = 10
+
+console.log(cup)
+console.log(collegeBag)
+
+// let cup = "coffee" // string
+// let collegeBag = 'book' // string
+// let numberOfChauChau = 2 // integer
+// let isNepali = true // boolean
+
+// const cup = "coffee" // string
+// const collegeBag = 'book' // string
+// const numberOfChauChau = 2 // integer
+// const isNepali = true // boolean
