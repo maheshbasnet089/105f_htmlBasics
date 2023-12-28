d34e2d23a199fdb7d994ea63951a18a38b3e085c
Manish Basnet <manishbasnet@Manishs-MacBook-Air.local>
2023-12-28 20:14:37 +0545

    learn about variabes,array,object

---
+++ week3/day2.js
@@ -0,0 +1,49 @@
+// var variableName = "value"
+// let variableName2 = "value"
+// const variableName3 = ""
+
+var name1 = "Manish Basnet"
+var name2= "Bob Marley"
+var name3 = "Ramesh Adhikari"
+
+
+// console.log(name1)
+// console.log(name2)
+// console.log(name3)
+
+//ARRAY 
+var names = ['Manish Basnet','Ramesh Adhikari',12]
+names[0] = "Mahesh Basnet"
+
+// console.log(names)
+
+// console.log(names[0])
+// console.log(names[1])
+// console.log(names[2])
+
+
+// objects 
+
+var person1 = {
+    name : "Manish Basnet",
+    age : 21,
+    address : "Itahari"
+}
+Object.freeze(person1)
+
+person1.name = "Jitu Thakur"
+console.log(person1)
+// person1.age = 15
+// delete person1.address
+
+// console.log(person1)
+
+
+
+// console.log(person1)
+// console.log(person1.name,person1.age,person1.address)
+// console.log(person1.age)
+// console.log(person1.address)
+
+// console.log(person1['name'])
+// console.log(person1['age'])
