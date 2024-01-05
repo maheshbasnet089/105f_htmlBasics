d51290ac4487535d84a48f841a227fb18639ff24
Manish Basnet <manishbasnet@Manishs-MacBook-Air.local>
2024-01-05 20:06:49 +0545

    complete week3

---
+++ week3/day6.js
@@ -0,0 +1,12 @@
+const h1 =  document.querySelector('.catchme')
+const button = document.querySelector(".clickMeButton")
+h1.textContent = "Learning C"
+h1.style.backgroundColor  = 'blue'
+
+// button.addEventListener("mouseleave",function(){
+//     h1.textContent = "Learning Click me"
+// })
+
+function changeTextContent(){
+    alert("You Clicked Me")
+}
